dd13d31cd5b1600c753edc9f65a7ff8dccd0817f
Mike Brady <brady@cs.tcd.ie>
2018-03-07 18:20:08 +0000

    Add code to print a line and add some consumer thread code (incomplete)

--- pandc.c
+++ pandc.c
@@ -1,16 +1,18 @@
 #include <pthread.h>
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
+
+// This is as of March 7, 2018
 
 #define NUM_CONSUMER_THREADS 3
 
 struct mc {
-  char* str;
+  char *str;
   int new;
 };
 
 struct cp {
-  char* str;
+  char *str;
   int index;
   int new;
 };
@@ -24,11 +26,19 @@ pthread_mutex_t cpm = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t mccv = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cpcv = PTHREAD_COND_INITIALIZER;
 
-
 void *consumerThreadFunction(void *stuff) {
   int *p = (int *)stuff;
   int index = *p;
-  printf("Consumer thread index is %d.\n",index);
+  printf("Consumer thread index is %d.\n", index);
+  while (1) {
+    pthread_mutex_lock(&mcm);
+    while (mtoc.new == 0)
+      pthread_cond_wait(&mccv, &mcm);
+    char *s = mtoc.str;
+    mtoc.new = 0;
+    pthread_mutex_unlock(&mcm);
+    printf("Consumer %d has the string \"%s\".\n", index, s);
+  }
   pthread_exit(NULL);
 }
 
@@ -37,42 +47,52 @@ void *printerThreadFunction(void *stuff) {
   pthread_exit(NULL);
 }
 
-int main (int argc, const char * argv[]) { 
-	pthread_t threads[NUM_CONSUMER_THREADS];
-	int indexes[NUM_CONSUMER_THREADS]; 
-	int rc,t; 
-	for (t=0;t<NUM_CONSUMER_THREADS;t++) { 
-		printf("Creating thread %d\n",t);
-		indexes[t] = t;
-		rc = pthread_create(&threads[t],NULL,consumerThreadFunction,(void *)&indexes[t]); 
-		if (rc) { 
-			printf("ERROR return code from pthread_create() of consumer thread: %d\n",rc); 
-			exit(-1); 
-		} 
-	}
-	
-	pthread_t printerThread;
-	rc = pthread_create(&printerThread,NULL,printerThreadFunction,NULL); 
-	if (rc) { 
-		printf("ERROR return code from pthread_create() of the printer thread: %d\n",rc); 
-		exit(-1); 
-	}
-	
-  pthread_mutex_lock(&mcm);
-  
-  // this is as far as we got, March 6, 2018.
-  /*
-  int pthread_mutex_trylock(pthread_mutex_t *mutex);
-  int pthread_mutex_unlock(pthread_mutex_t *mutex);
-  Pthread Condition Variables
-  int pthread_cond_wait(pthread_cond_t *cond,
-                        pthread_mutex_t *mutex);
-  int pthread_cond_signal(pthread_cond_t *cond);
-  */
+int main(int argc, const char *argv[]) {
 
-	// wait for threads to exit 
-	for(t=0;t<NUM_CONSUMER_THREADS;t++) { 
-		pthread_join( threads[t], NULL); 
-	} 
-	return 0;
+  mtoc.new = 0;
+  ctop.new = 0;
+
+  pthread_t threads[NUM_CONSUMER_THREADS];
+  int indexes[NUM_CONSUMER_THREADS];
+  int rc, t;
+  for (t = 0; t < NUM_CONSUMER_THREADS; t++) {
+    printf("Creating thread %d\n", t);
+    indexes[t] = t;
+    rc = pthread_create(&threads[t], NULL, consumerThreadFunction,
+                        (void *)&indexes[t]);
+    if (rc) {
+      printf("ERROR return code from pthread_create() of consumer thread: %d\n",
+             rc);
+      exit(-1);
+    }
+  }
+
+  pthread_t printerThread;
+  rc = pthread_create(&printerThread, NULL, printerThreadFunction, NULL);
+  if (rc) {
+    printf(
+        "ERROR return code from pthread_create() of the printer thread: %d\n",
+        rc);
+    exit(-1);
+  }
+
+  while (1) {
+    size_t n = 0;
+    char *lineptr = NULL;
+    ssize_t len = getline(&lineptr, &n, stdin);
+
+    printf("A string of %ld characters was read: \"%s\".\n", len, lineptr);
+
+    pthread_mutex_lock(&mcm);
+    mtoc.str = lineptr;
+    mtoc.new = 1;
+    pthread_cond_signal(&mccv);
+    pthread_mutex_unlock(&mcm);
+  }
+
+  // wait for threads to exit
+  for (t = 0; t < NUM_CONSUMER_THREADS; t++) {
+    pthread_join(threads[t], NULL);
+  }
+  return 0;
 }
